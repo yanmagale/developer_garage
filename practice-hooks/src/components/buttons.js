625b173bf933e5a3a5e4a78555979e8dee21f917
Yan Magalhães <yanmagale@gmail.com>
2020-06-24 00:28:08 -0300

    feat: add array to prevent render unnecessary

--- practice-hooks/src/components/buttons.js
+++ practice-hooks/src/components/buttons.js
@@ -3,14 +3,21 @@ import React, { useState, useEffect } from 'react';
 const Button = () => {
   // Declare a new state variable, which we'll call "count"
   const [clickedTimes, updateClickedTimes] = useState(0);
+  const [isOnline, setIsOnline] = useState(null);
 
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
     // Update the document title using the browser API
+    console.log('Effect 1')
     document.title = `You clicked ${clickedTimes} times`;
+  }, [clickedTimes]);
 
-    return () => {console.log(clickedTimes)};
-  });
+  useEffect(() => {
+    console.log('Effect 2')
+    setIsOnline(true)
+
+    return () => {console.log('true')}
+  }, [isOnline])
 
   const updateNumber = (number) => updateClickedTimes(number ** 2); 
 
